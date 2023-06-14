bf055a7f20c5dc85df78c4c1d71cf118024503b2
Fabien Chavonet <fchavonet@mailo.com>
2023-06-14 10:34:51 -0700

    Try to solve problemes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Main program function
- * Return: 0 if success
+ * main - test
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("Size of a char: %c byte(s)\n", sizeof(char);
-	printf("Size of an int: %d byte(s)\n", sizeof(int);
-	printf("Size of a long int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long long int: %ld byte(s)\n",sizeof(long);
+	printf("Size of a char: %c byte(s)\n", sizeof(char));
+	printf("Size of a an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %f byte(s)\n", sizeof(float));
 	return (0);
 }
