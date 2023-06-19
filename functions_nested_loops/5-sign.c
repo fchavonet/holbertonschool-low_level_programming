1dc1515d7fd6ab00b5514b95ff799de74ba28080
Fabien Chavonet <fchavonet@mailo.com>
2023-06-19 13:53:42 -0700

    5. Sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of the number
+ *@n: number to be checked
+ *
+ * Return: 1 if n > 0, 0 if n = 0, -1 if n < 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
