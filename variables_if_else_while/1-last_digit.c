f32a0d801a443d6db2a6193598a85c5b932d9d7d
Fabien Chavonet <fchavonet@mailo.com>
2023-06-19 00:44:36 -0700

    Correction of a line (replace 5 by 0)

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
-	else if (l == 5)
+	else if (l == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, l);
 	}
