893d54b29621ba51e40165487a6483a121cfc1ba
Fabien Chavonet <fchavonet@mailo.com>
2023-06-19 02:32:56 -0700

    5. Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number = 0;
+
+	/* Display 123456789 and stop before 10 */
+	for (number = 0 ; number < 10 ; number++)
+	{
+
+		printf("%d", number);
+
+	}
+	putchar('\n');
+	return (0);
+}
