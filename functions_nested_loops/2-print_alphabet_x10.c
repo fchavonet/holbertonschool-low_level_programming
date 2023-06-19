44d267c029447a19dea2864f8cf80266bb0dfbc9
Fabien Chavonet <fchavonet@mailo.com>
2023-06-19 12:48:22 -0700

    2. 10 x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Fonction pour imprimer l'alphabet en minuscules x10
+ */
+
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	int nb_repeat;
+
+	for (nb_repeat = 0 ; nb_repeat <= 10 ; nb_repeat++)
+	{
+		for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+		{
+			_putchar(alphabet);
+			_putchar('\n');
+		}
+	}
+}
