88fd0831a2f2d61d7a39a4ae412e9a5c0f236680
Fabien Chavonet <fchavonet@mailo.com>
2023-06-19 13:14:06 -0700

    Adding fonction manual

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _islower - check if letter is lowercase
+ *@c: character to be checked
+ *
+ * Return: 1 if it's good, 0 in other case
+ */
+
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
