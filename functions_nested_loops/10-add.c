a758054ae508f0b64ad1fffd4a736d26cc37708d
Fabien Chavonet <fchavonet@mailo.com>
2023-06-20 08:56:56 -0700

    10. a + b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add two integers and return result
+ *
+ *@a: first number to do the add
+ *@b: second number to do the add
+ *
+ * Return: the result of a + b
+ */
+
+int add(int a, int b)
+{
+	int result = (a + b);
+
+	return (result);
+}
