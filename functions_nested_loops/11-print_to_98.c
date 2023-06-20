b532d72b022127a91917df41b2705e520e398ec4
Fabien Chavonet <fchavonet@mailo.com>
2023-06-20 12:17:15 -0700

    11. 98 Battery Street, the OG (and save of a crazy work I do because I don't read enough the instructions

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_to_98(int n)
+{
+	for (; n < 98 ; n++)
+	{
+		printf("%d,", n);
+	}
+	for (; n > 98 ; n--)
+	{
+		printf("%d,", n);	
+	}
+	printf("%d\n", n);
+}
