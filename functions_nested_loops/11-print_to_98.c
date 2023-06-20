0087314bd01d96bcc0039251f85506e6ab99ed39
Fabien Chavonet <fchavonet@mailo.com>
2023-06-20 12:21:04 -0700

    Betty correction and adding a ' ' after ,

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ *@n: number to start the count
+ */
+
 void print_to_98(int n)
 {
 	for (; n < 98 ; n++)
 	{
-		printf("%d,", n);
+		printf("%d, ", n);
 	}
 	for (; n > 98 ; n--)
 	{
-		printf("%d,", n);	
+		printf("%d, ", n);
 	}
 	printf("%d\n", n);
 }
