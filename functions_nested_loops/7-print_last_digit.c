ddd9dfa94918b126ae551c84cb6d4f87309f6e8c
Fabien Chavonet <fchavonet@mailo.com>
2023-06-20 01:00:46 -0700

    New try, don't understand what that doesn't work

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -4,21 +4,23 @@
  * print_last_digit - print thelast digit of n
  *@n: number to be checked
  *
- * Return: 0 if it's good
+ * Return: n if it's good
  */
 
 int print_last_digit(int n)
 {
+	int last_digit = 0;
+
 	if (n < 0)
 	{
-		n = -1 * n % 10;
-		_putchar(n);
-		return (0);
+		last_digit = -1 * n % 10;
+		_putchar(last_digit);
+		return (last_digit);
 	}
 	else
 	{
-		n = n % 10;
-		_putchar(n);
-		return (0);
+		last_digit = n % 10;
+		_putchar(last_digit);
+		return (last_digit);
 	}
 }
