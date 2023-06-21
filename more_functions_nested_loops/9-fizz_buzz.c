4ace622fb3edf7eec9b1c7f4483072c112068262
Fabien Chavonet <fchavonet@mailo.com>
2023-06-21 12:58:32 -0700

    9. Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz-Buzz
+ *
+ * Return: 0 if the code works well
+ */
+
+int main(void)
+{
+
+	int i = 0;
+	int multipl3 = 0;
+	int multipl5 = 0;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+
+		multipl3 = i % 3;
+		multipl5 = i % 5;
+
+		if ((multipl3 == 0) && (multipl5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+
+		else if (multipl3 == 0)
+		{
+			printf("Fizz ");
+		}
+
+		else if (multipl5 == 0)
+		{
+			printf("Buzz ");
+		}
+
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+
+	return (0);
+}
