98b15c39f3f652407161e45b4b2663caf3b12c6a
Fabien Chavonet <fchavonet@mailo.com>
2023-06-21 13:06:24 -0700

    Solve the problem of the space at the end

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -21,22 +21,26 @@ int main(void)
 
 		if ((multipl3 == 0) && (multipl5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 
 		else if (multipl3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 
 		else if (multipl5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 
 		else
 		{
 			printf("%d ", i);
+		}
+		if (i < 100)
+		{
+		printf(" ");
 		}
 	}
 	printf("\n");
