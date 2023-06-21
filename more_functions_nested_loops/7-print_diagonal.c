d37b19946e7392d96acdfc8407a9c3b6d230c0d4
Fabien Chavonet <fchavonet@mailo.com>
2023-06-21 11:51:32 -0700

    Correction of upload problem

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ *
+ * @n: n is the number of times the character \ should be printed
+ */
+
+void print_diagonal(int n)
+{
+	int i, j = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (i = 0 ; i < n ; i++)
+	{
+		for (j = 0 ; j < n ; j++)
+		{
+			if (i == j)
+			{
+				_putchar(92);
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+	_putchar ('\n');
+	}
+}
+
