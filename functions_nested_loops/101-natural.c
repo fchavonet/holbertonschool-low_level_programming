7d82b749affa3a86a8a3fbe1f9fdbdbac82407b8
Fabien Chavonet <fchavonet@mailo.com>
2023-06-22 05:51:04 -0700

    Betty correction

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 
+/**
+ * main - enter point of the programm
+ *
+ * Return: 0 if programm works
+ */
+
 int main(void)
 {
 	int i = 0;
 	int sum = 0;
 
 	for (i = 1 ; i <= 1024 ; i++)
+
 	{
 		if (((i % 3) == 0) || ((i % 5) == 0) || ((i % 6) == 0) || ((i % 9) == 0))
 		{
+
 			sum += i;
+
 		}
+
 	}
+
 	printf("%d\n", sum);
+
+	return (0);
 }
