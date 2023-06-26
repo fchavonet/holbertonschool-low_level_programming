2960c361db040ffbad7ebbde72b98d81df5af010
Fabien Chavonet <fchavonet@mailo.com>
2023-06-26 10:05:56 -0700

    New betty corrections (space before ,)

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * rev_string - reverse string
+ *
+ * @s: the string to reverse
+ */
+
 void rev_string(char *s)
 {
 	int length = 0;
@@ -14,10 +20,11 @@ void rev_string(char *s)
 
 	/* Après cette étape, length = 9 si s = My School */
 
-	for (a = 0, b = length - 1; a <= (length / 2) ; a++ , b--)
+	for (a = 0, b = length - 1 ; a <= (length / 2) ; a++, b--)
 	{
 		temp = s[a];
 		s[a] = s[b];
 		s[b] = temp;
 	}
+	_putchar('\n'à;
 }
