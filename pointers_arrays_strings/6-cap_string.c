e017c257829584b8dbc52c9370852dd186cda8b4
Fabien Chavonet <fchavonet@mailo.com>
2023-06-28 00:44:32 -0700

    Correction of capitalized first letter

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -14,10 +14,24 @@ char *cap_string(char *str)
 	{
 		if (str[index] >= 'a' && str[index] <= 'z')
 		{
-			if (str[index - 1] == ' ' || (str[index -1]) == '\t' || (str[index -1]) == '\n' || (str[index -1]) == ',' || (str[index -1]) == (';') || (str[index -1]) == ('.') || (str[index -1]) == '!' || (str[index -1]) == '?' || (str[index -1]) == '"' || (str[index -1]) == '(' || (str[index -1]) == ')' || (str[index -1]) == '{' || (str[index -1]) == ('}'))
-			{
-				(str[index]) -= 32;
-			}
+			if
+				((str[index - 1]) == ' ' ||
+				 (str[index -1]) == '\t' ||
+				 (str[index -1]) == '\n' ||
+				 (str[index -1]) == ',' ||
+				 (str[index -1]) == (';') ||
+				 (str[index -1]) == ('.') ||
+				 (str[index -1]) == '!' ||
+				 (str[index -1]) == '?' ||
+				 (str[index -1]) == '"' ||
+				 (str[index -1]) == '(' ||
+				 (str[index -1]) == ')' ||
+				 (str[index -1]) == '{' ||
+				 (str[index -1]) == ('}') ||
+				 (index == 0))
+				{
+					(str[index]) -= 32;
+				}
 		}
 		index++;
 	}
