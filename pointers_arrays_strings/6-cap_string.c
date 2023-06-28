dba6a6c751049f04001f631987921d812689fee1
Fabien Chavonet <fchavonet@mailo.com>
2023-06-28 00:28:44 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[index] != '\0')
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+		{
+			if (str[index - 1] == ' ' || (str[index -1]) == '\t' || (str[index -1]) == '\n' || (str[index -1]) == ',' || (str[index -1]) == (';') || (str[index -1]) == ('.') || (str[index -1]) == '!' || (str[index -1]) == '?' || (str[index -1]) == '"' || (str[index -1]) == '(' || (str[index -1]) == ')' || (str[index -1]) == '{' || (str[index -1]) == ('}'))
+			{
+				(str[index]) -= 32;
+			}
+		}
+		index++;
+	}
+
+	return (str);
+}
