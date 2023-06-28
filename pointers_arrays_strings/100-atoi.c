b8e8c5c50fdcb968297e14328e9e5a05cb560819
Fabien Chavonet <fchavonet@mailo.com>
2023-06-28 12:23:30 -0700

    Second step of the task 10, find solutiopn for negative number

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,7 +3,7 @@
 
 /**
  * _atoi - convert a string to an integer
- * 
+ *
  * @s: a rediger
  *
  * Return: result is final integer
@@ -13,10 +13,16 @@ int _atoi(char *s)
 {
 	int index = 0;
 	int result = 0;
-
+	int number_neg = 0;
+	int sign = 0;
 
 	while (s[index] != '\0')
 	{
+		if (s[index] == '-')
+		{
+			number_neg += 1;
+		}
+
 		if (s[index] >= '0' && s[index] <= '9')
 		{
 			result = result * 10 + s[index] - '0';
@@ -24,5 +30,14 @@ int _atoi(char *s)
 		index++;
 	}
 
-	return (result);
+	if (number_neg % 2 == 0)
+	{
+		sign = 1;
+	}
+	else
+	{
+		sign = -1;
+	}
+
+	return (result * sign);
 }
