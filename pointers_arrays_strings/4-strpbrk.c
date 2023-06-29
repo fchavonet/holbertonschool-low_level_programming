8b28c754940e89a3599f5f5845f6fa189efc5eb0
Fabien Chavonet <fchavonet@mailo.com>
2023-06-29 09:03:44 -0700

    First upload of task: 4. strpbrk

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,3 +1,26 @@
 #include "main.h"
 
+/**
+ * _strpbrk -  search a string for any of a set of bytes
+ *
+ * @s: the string to test
+ * @accept: the strign for reference
+ *
+ * Return: s + 1
+ */
 
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == accept[i])
+		{
+			s++;
+		}
+		i++;
+	}
+
+	return (s + 1);
+}
