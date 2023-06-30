e15800df64123879d4bf8386d88f2f814bdd05ec
Fabien Chavonet <fchavonet@mailo.com>
2023-06-30 01:02:57 -0700

    Think I have my solution, adding c == '\0' in condition

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -14,18 +14,13 @@ char *_strchr(char *s, char c)
 
 	int index = 0;
 
-	while (s[index] != '\0')
+	while (s[index] != '\0' || c == '\0')
 	{
 		if (s[index] == c)
 		{
 			return (s + index);
 		}
 		index++;
-	}
-
-	if (s[index] != c)
-	{
-		return (0);
 	}
 
 	return (0);
