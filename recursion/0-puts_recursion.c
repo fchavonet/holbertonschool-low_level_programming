df158a7dc0adea0995f965c2c020596500c1fb60
Fabien Chavonet <fchavonet@mailo.com>
2023-07-03 01:48:59 -0700

    Review all my code after bad modifications

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -9,14 +9,14 @@
 void _puts_recursion(char *s)
 {
 
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
-
 	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 
 }
