88b549f38e881b029e7842807ecff9da6de11cbe
Fabien Chavonet <fchavonet@mailo.com>
2023-07-03 01:24:35 -0700

    First upload of the task: 0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print the letter where the pointer is
+ *
+ * @s: string to print
+ */
+
+void _puts_recursion(char *s)
+{
+
+	_putchar(*s);
+	s++;
+
+	if (*s != '\0')
+	{
+		_puts_recursion(s);
+	}
+
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+
+}
