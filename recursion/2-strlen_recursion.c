2e987c5280921699083e27790695d85e6abb7935
Fabien Chavonet <fchavonet@mailo.com>
2023-07-03 05:38:46 -0700

    Betty correction

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,11 +1,18 @@
 #include "main.h"
 
+/**
+ * _strlen_recursion - return length of a string
+ *
+ * @s: string to count
+ */
+
+
 int _strlen_recursion(char *s)
 {
 
 	if (*s != '\0')
 	{
-		return (1+ _strlen_recursion(s + 1));
+		return (1 + _strlen_recursion(s + 1));
 	}
 	return (0);
 
