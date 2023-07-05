0b79c17f46a1ae13b91dcbe7442703b984249d08
Fabien Chavonet <fchavonet@mailo.com>
2023-07-05 01:52:37 -0700

    First upload of task: 3. Neither irony nor sarcasm is argument

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ *
+ * @argc: number of arguments
+ * @argv: array of character pointers listing all the arguments
+ *
+ * Return: 0 when the main fonction end
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+
+		int  multi_a = atoi(argv[1]);
+		int  multi_b = atoi(argv[2]);
+
+		int result = 0;
+
+		result = multi_a * multi_b;
+
+		printf("%d\n", result);
+	}
+
+	else
+	{
+		printf("Error");
+	}
+
+	return (0);
+
+}
