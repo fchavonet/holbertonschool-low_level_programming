ce67bd75e558ca9513e0489cacc46bd76bd1365a
Fabien Chavonet <fchavonet@mailo.com>
2023-07-05 06:50:15 -0700

    Betty correction

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - adds numbers
+ *
+ * @argc: number of arguments
+ * @argv: array of character pointers listing all the arguments
+ *
+ * Return: 0 when the main fonction end
+ */
+
+
 int main(int argc, char *argv[])
 {
-	/**
-	 * main - adds numbers
-	 *
-	 * @argc: number of arguments
-	 * @argv: array of character pointers listing all the arguments
-	 *
-	 * Return: 0 when the main fonction end
-	 */
-
 	int index1 = 1;
 	int index2 = 0;
-
 	char *s;
-
 	int result = 0;
 
 	while (index1 < argc)
 	{
 		s = argv[index1];
 		index2 = 0;
-
-		while (s[index2] !='\0')
+		while (s[index2] != '\0')
 		{
 			if (s[index2] < '0' || s[index2] > '9')
 			{
@@ -39,10 +37,7 @@ int main(int argc, char *argv[])
 	for (index1 = 1 ; index1 < argc ; index1++)
 	{
 		result = result + atoi(argv[index1]);
-	
 	}
-
 	printf("%d\n", result);
-
 	return (0);
 }
