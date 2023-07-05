6fa20098d6076ad9321f44f55dbdd3904bf314cf
Fabien Chavonet <fchavonet@mailo.com>
2023-07-05 00:48:51 -0700

    Add -1 not to include the name

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - prints the program name
+ *
+ * @argc: number og arguments
+ * @argv: array of character pointers listing all the arguments
+ *
+ * Return: 0 when the main fonction end
+ */
+
 int main(int argc, char *argv[] __attribute__ ((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
