a16e6d5932b76f725203d981f44c5961dbfc4a47
Fabien Chavonet <fchavonet@mailo.com>
2023-07-10 06:27:04 -0700

    removing useless {} for betty, to much line in the code

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,49 +14,32 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int size_s1 = 0;
-	int size_s2 = 0;
+	int size_s1, size_s2;
 	char *result = NULL;
-	int i = 0;
-	int j = 0;
+	int i, j;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
+
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	while (s1[size_s1] != '\0')
-	{
 		size_s1++;
-	}
 
 	while (s2[size_s2] != '\0')
-	{
 		size_s2++;
-	}
 
 	result = malloc((size_s1 + size_s2 + 1) * sizeof(char));
-	
-	
 
 	if (result == NULL)
-	{
 		return (NULL);
-	}
 
 	for (i = 0; i < size_s1; i++)
-	{
 		result[i] = s1[i];
-	}
 
 	for (j = 0; j < size_s2; j++)
-	{
 		result[i + j] = s2[j];
-	}
 
 	result[i + j] = '\0';
 
