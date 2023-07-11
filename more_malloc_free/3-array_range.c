35d5640c6dabd7526611a95d23b39e4237bd54b2
Fabien Chavonet <fchavonet@mailo.com>
2023-07-11 06:37:03 -0700

    Always try to find soluition, work on local

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -16,14 +16,14 @@ int *array_range(int min, int max)
 
 	int *array = NULL;
 	int index = 0;
-	char n = 0;
+	int n = 0;
 
 	if (min > max)
 	{
 		return (NULL);
 	}
 
-	array = malloc((max - min) * sizeof(int));
+	array = malloc((max - min + 1) * sizeof(int));
 
 	if (array == NULL)
 	{
