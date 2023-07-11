608bea7455e3c7585d7bf7c2e8be71a781a478bb
Fabien Chavonet <fchavonet@mailo.com>
2023-07-11 05:15:46 -0700

    First upload of task: 2. _calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "main.h"
+
+/**
+ * _calloc -
+ *
+ * @nmemb:
+ * @size:
+ *
+ * Return:
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+	int *array = NULL;
+	unsigned int index = 0;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	array = malloc(nmemb * size);
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	while (index < (nmemb * size))
+	{
+		array[index] = 0;
+		index++;
+	}
+
+	return (array);
+
+}
