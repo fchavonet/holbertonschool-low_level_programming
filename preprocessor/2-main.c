527295c883bbca0ce369893fbea6a241d0d9f827
Fabien Chavonet <fchavonet@mailo.com>
2023-07-12 00:24:11 -0700

    First upload os task: 2. File name

---
+++ preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "main.h"
+
+/**
+ * main - print the name of the file
+ *
+ * Return: 0 when progam is done
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
