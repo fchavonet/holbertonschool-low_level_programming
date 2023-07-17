8dddde4a9c05e0be7af90d86cbca2ac39a027a6e
Fabien Chavonet <fchavonet@mailo.com>
2023-07-17 06:20:52 -0700

    Fix: memory leaks and catch if name or owner is NULL

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "dog.h"
 
@@ -17,6 +18,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 
 	dog_t *dog;
+	int size_name = 0;
+	int size_owner = 0;
 
 	dog = malloc(sizeof(dog_t));
 
@@ -25,16 +28,42 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return(NULL);
 	}
 
-	if (name == NULL || owner == NULL)
+	/* malloc and copy of name */
+
+	while(name[size_name] != '\0')
 	{
+		size_name++;
+	}
+
+	dog->name = malloc(sizeof(char) * (size_name + 1));
+
+	if (dog->name == NULL)
+	{
+		free(dog);
 		return(NULL);
 	}
 
-	if (owner == NULL)
+	strcpy(dog->name, name);
 
-	dog->name = name;
+	/* malloc and copy of owner */
+
+	while(owner[size_owner] != '\0')
+	{
+		size_owner++;
+	}
+
+	dog->owner = malloc(sizeof(char) * (size_owner + 1));
+
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return(NULL);
+	}
+
+	strcpy(dog->owner, owner);
+
 	dog->age = age;
-	dog->owner = owner;
 
 	return (dog);
 }
