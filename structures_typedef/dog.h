4ab1b89611cf2380b07714cc736e6d4dd1a2f124
Fabien Chavonet <fchavonet@mailo.com>
2023-07-17 01:38:42 -0700

    Adding new type dog_t as a new name for the type struct dog in dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -10,12 +10,12 @@
  *
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 /* function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
