75c8a99d75ffe687c727cca6fdc54a9101d40fa3
Fabien Chavonet <fchavonet@mailo.com>
2023-07-17 01:29:35 -0700

    Add () for output (nill)

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -18,12 +18,12 @@ void print_dog(struct dog *d)
 
 	if (d->name == NULL)
 	{
-		d->name = "nil";
+		d->name = "(nil)";
 	}
 
 	if (d->owner == NULL)
 	{
-		d->owner = "nil";
+		d->owner = "(nil)";
 	}
 
 	printf("Name: %s\n", d->name);
