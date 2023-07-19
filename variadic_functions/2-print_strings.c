e4cbe5b04e1072c1cfc6844facf69e41167b4602
Fabien Chavonet <fchavonet@mailo.com>
2023-07-19 01:34:26 -0700

    Fix: () around nil, condition if separato == NULL

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -21,18 +21,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
+	if (separator == NULL)
+	{
+		separator = "";
+	}
+
 	for (; index < n ; index++)
 	{
 		string = va_arg(args, char *);
 
 		if (string == NULL)
 		{
-			printf("nil");
-		}
-
-		if (separator == NULL)
-		{
-			printf("%s", string);
+			printf("(nil)");
 		}
 		else
 		{
