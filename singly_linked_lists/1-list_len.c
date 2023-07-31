86b2498394fd6e4d51235cc8e68747bb4c3034f5
Fabien Chavonet <fchavonet@mailo.com>
2023-07-31 04:52:58 -0700

    betty correction on 1-list_len.c

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,8 +1,16 @@
 #include "lists.h"
 
+/**
+ * list_len - prints the number of elements in the list
+ *
+ * @h: pointer to the first node
+ *
+ * Return: the number of node
+ */
+
 size_t list_len(const list_t *h)
 {
-	int number_nodes;
+	int number_nodes = 0;
 
 	while (h != NULL)
 	{
