721ed3aff9a3c6291eb9d1c384d35f9770ab1670
Fabien Chavonet <fchavonet@mailo.com>
2023-07-31 03:18:56 -0700

    first upload of the task: 0. Print list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list
+ *
+ * @h: pointer to the first node
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodes_number = 0;
+
+	while (h != NULL)
+	{
+		if (h->str  == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+
+		nodes_number++;
+		h = h->next;
+
+	}
+
+	return (nodes_number);
+}
