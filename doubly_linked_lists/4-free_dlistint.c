f55352556a4b1585433a4fc6d593451bcdea9e75
Fabien Chavonet <fchavonet@mailo.com>
2023-08-01 00:00:22 -0700

    betty correction (adding function comment

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * free_dlistint - frees the dlistint_t list
+ *
+ * @head: pointer to the pointer of the first node
+ */
 void free_dlistint(dlistint_t *head)
 {
 	if (head != NULL)
