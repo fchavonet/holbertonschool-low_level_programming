a1131f8be7ccbf8df4f0e4651d45c1c23642dd42
Fabien Chavonet <fchavonet@mailo.com>
2023-08-01 06:33:27 -0700

    try to solve the checker problem, optimize the code for betty pass...

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -4,50 +4,46 @@
  * delete_dnodeint_at_index - delete a node at a given position
  *
  * @head: pointer to the pointer of the first node
- * @index: the index when the node should be delete (before)
+ * @index: the index when the node should be deleted (starting from 0)
  *
- * Return: 1 if it succeeded
- *         -1 if it failed
+ * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *previous_node = NULL;
-	dlistint_t *actual_node = *head;
-	dlistint_t *next_node = NULL;
-	unsigned int actual_index = 0;
+	dlistint_t *current_node, *prev_node, *next_node;
+	unsigned int actual_index;
 
 	if (*head == NULL)
 		return (-1);
 
+	current_node = *head;
+
 	if (index == 0)
 	{
 		*head = (*head)->next;
-
-		if (*head != NULL)
+		if (*head)
 			(*head)->prev = NULL;
-		free(actual_node);
+		free(current_node);
 		return (1);
 	}
 
 	for (actual_index = 0; actual_index < index; actual_index++)
 	{
-		actual_node = actual_node->next;
-		if (actual_node == NULL)
+		if (current_node == NULL)
 			return (-1);
-		if (!actual_node->next)
-		{
-			actual_node->prev = NULL;
-			actual_node->next = NULL;
-			free(actual_node);
-			return (1);
-		}
 
-		previous_node = actual_node->prev;
-		next_node = actual_node->next;
-		previous_node->next = next_node;
-		next_node->prev = previous_node;
-		free(actual_node);
+		current_node = current_node->next;
 	}
 
+	prev_node = current_node->prev;
+	next_node = current_node->next;
+
+	if (prev_node)
+		prev_node->next = next_node;
+	if (next_node)
+		next_node->prev = prev_node;
+
+	free(current_node);
 	return (1);
 }
+
