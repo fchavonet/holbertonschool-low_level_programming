1dbaa472f9df56436f580bb60ba618746b349c7e
Fabien Chavonet <fchavonet@mailo.com>
2023-08-02 10:57:26 -0700

    always adapt better solution from task 1 to other task (code is easiest to understand

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -9,14 +9,12 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int mask = 1;
-
-	if (index >= (sizeof(unsigned long int) * 8))
+	if (index > 64)
+	{
 		return (-1);
+	}
 
-	mask <<= index;
+	*n = ((1 << index) | *n);
 
-	*n |= mask;
 	return (1);
 }
-
