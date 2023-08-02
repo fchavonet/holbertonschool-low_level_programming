80c0dcdb40e104105362552875f7f28981205219
Fabien Chavonet <fchavonet@mailo.com>
2023-08-02 06:08:20 -0700

    Trying to fix the segmentation fault error.

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -25,7 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	if (idx == 0)
-		return (add_dnodeint(h, n));
+		return (new_node);
 
 	if (*h == NULL)
                 return (NULL);
