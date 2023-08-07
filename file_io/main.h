7fd8f9f6ab8dfed6c072257c5af2f835b0a24e2b
Fabien Chavonet <fchavonet@mailo.com>
2023-08-07 06:54:19 -0700

    adding main.h file (forgot to push it)

--- file_io/main.h
+++ file_io/main.h
@@ -3,9 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
