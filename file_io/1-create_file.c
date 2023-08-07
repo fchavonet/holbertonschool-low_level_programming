3c5b1cbf6c08ab69919698859d87b69726dd66dd
Fabien Chavonet <fchavonet@mailo.com>
2023-08-07 07:28:00 -0700

    just a test (= '\0')

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		while (text_content[length])
+		while (text_content[length] != '\0')
 			length++;
 	}
 
