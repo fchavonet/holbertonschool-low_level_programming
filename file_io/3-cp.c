da26efea97dcd1f85dd07e8648a8177691165480
Fabien Chavonet <fchavonet@mailo.com>
2023-08-08 00:36:09 -0700

    first uploàad of task: 3. cp

---
+++ file_io/3-cp.c
@@ -0,0 +1,76 @@
+#include "main.h"
+
+/**
+ * main - copies the contents of one file to another
+ *
+ * @argc: the number of command-line arguments
+ * @argv: an array of strings containing the command-line arguments
+ *
+ * Return: 0 on success
+ *         or an appropriate error code on failure
+ */
+int main(int argc, char *argv[])
+{
+	int file_from;
+	int file_to;
+	ssize_t bytes_read;
+	ssize_t bytes_written;
+	char buffer[1024];
+
+	char *prog_name = "cp";
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", prog_name);
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(file_from);
+		exit(99);
+	}
+
+	while ((bytes_read = read(file_from, buffer, sizeof(buffer))) > 0)
+	{
+		bytes_written = write(file_to, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(file_from);
+			close(file_to);
+			exit(99);
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(file_from);
+		close(file_to);
+		exit(98);
+	}
+
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+
+	return (0);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -3,7 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <libgen.h>
 #include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
