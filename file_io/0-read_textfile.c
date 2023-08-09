592d3156dbc5d593d68119513914162acc855fc5
Fabien Chavonet <fchavonet@mailo.com>
2023-08-09 02:11:50 -0700

    adding some verificator for read and write

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -6,44 +6,49 @@
  * @filename: the file to read
  * @letters: the number of letters
  *
- * Return: the actual number of letters
- *         0 if the file is NULL or can not be opened or read
+ * Return: the actual number of letters written to standard output
+ *         0 if the file is NULL or cannot be opened, read, or written
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-
-	int file = 0;
-	ssize_t bytes_read = 0;
-	ssize_t bytes_written = 0;
-
-	char *file_content = NULL;
-
-	file_content = malloc(letters);
-
-	if (file_content == NULL)
-		return (0);
+	ssize_t bytes_read;
+	ssize_t bytes_written;
+	char *file_content;
+	int file;
 
 	if (filename == NULL)
-	{
-		free(file_content);
 		return (0);
-	}
 
 	file = open(filename, O_RDONLY);
-
 	if (file == -1)
+		return (0);
+
+	file_content = malloc(letters);
+	if (file_content == NULL)
 	{
-		free(file_content);
+		close(file);
 		return (0);
 	}
 
 	bytes_read = read(file, file_content, letters);
+	if (bytes_read == -1)
+	{
+		free(file_content);
+		close(file);
+		return (0);
+	}
 
 	bytes_written = write(STDOUT_FILENO, file_content, bytes_read);
+	if (bytes_written == -1)
+	{
+		free(file_content);
+		close(file);
+		return (0);
+	}
 
 	close(file);
-
 	free(file_content);
 
 	return (bytes_written);
 }
+
